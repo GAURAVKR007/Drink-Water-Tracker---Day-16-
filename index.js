67e9958ec33193bcd0458579d48909afdda94b5b
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-09-09 09:35:34 +0530

    Added Functionality to big cup and project completed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const listers = document.getElementById('litres')
 const perecentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup,idx) => {
     cup.addEventListener('click',()=>{highlightCups(idx)})
 })
@@ -20,4 +22,29 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+
+    const totalCups = smallCups.length
+
+    if(fullCups === 0 ) {
+        perecentage.style.visibility = 'hidden'
+        perecentage.style.height = 0
+    }else {
+        perecentage.style.visibility = 'visible'
+        perecentage.style.height = `${fullCups / totalCups * 330}px`
+        perecentage.innerText = `${fullCups/ totalCups * 100}%`
+    }
+
+    if(fullCups === totalCups){
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    }else {
+        remained.style.visibility = 'visible'
+        listers.innerText = `${2-(250 * fullCups /1000)}L`
+    }
 }
